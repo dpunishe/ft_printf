9a24f725d896e3662d305f3aefff297e9850f153
Daemon Punisher <dpunishe@et-f2.kzn.21-school.ru>
2021-10-18 13:17:51 +0300

    v2

--- ft_printf/libft/ft_putnchar.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-void	ft_putnchar(char c, int n)
-{
-	while (n-- > 0)
-		ft_putchar(c);
-}
--- ft_printf/libft/ft_putstr.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void	ft_putstr(char const *c)
-{
-	ft_putstr_fd(c, 1);
-}
